a21c4f0a7b4045ea4506a5172bdded195b92f81e
emade <emade@EMO>
2025-07-28 16:06:18 +0300

    Add project files.

---
+++ Task/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+// Estimating of the cost carpet cleaning
+
+int pricepersmallcarpet = 25;
+int priceperlargecarpet = 35;
+float taxrate = 0.06F;
+
+Console.Write(" enter number of small carpets: ");
+int smallcarpetsnumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("enter numer of large carpets:");
+int largecarpetsnumber = Convert.ToInt32(Console.ReadLine());
+
+double cost = (smallcarpetsnumber * pricepersmallcarpet) + (largecarpetsnumber * priceperlargecarpet);
+double tax = taxrate * cost;
+double totalestimate = tax + cost;
+Console.WriteLine("Estimate for carpet cleaning service");
+Console.WriteLine($"Number of small carpets: {smallcarpetsnumber}");
+Console.WriteLine($"Number of large carpets:{largecarpetsnumber}");
+Console.WriteLine($"Price per small room: {pricepersmallcarpet}");
+Console.WriteLine($"Price per large room: {priceperlargecarpet}");
+Console.WriteLine($"Cost :{cost}");
+Console.WriteLine($"tax : {tax}");
+Console.WriteLine($"Total estimate: {totalestimate}");
+Console.WriteLine($"This estimate is valid for 30 days:");
